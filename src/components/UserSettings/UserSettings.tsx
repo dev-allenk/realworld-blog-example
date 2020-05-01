93335205d31da2e00a51d8d481490d63f1e03131
allenk <dev.allenk@gmail.com>
2020-05-01 18:32:26 +0900

    feat: input value 상태로 관리하는 기능 추가

--- src/components/UserSettings/UserSettings.tsx
+++ src/components/UserSettings/UserSettings.tsx
@@ -1,16 +1,53 @@
 import React from "react";
 import S from "./styles";
 import Button from "../Button";
+import useInput from "../../hooks/useInput";
+
+const initialValues = {
+  image: "",
+  username: "",
+  bio: "",
+  email: "",
+  password: "",
+};
 
 export default function UserSettings() {
+  const { inputValue, handleChange } = useInput(initialValues);
+  const { image, username, bio, email, password } = inputValue;
+
   return (
     <S.Wrapper>
       <h1>Your Settings</h1>
-      <S.Input placeholder={"URL of profile picture"} />
-      <S.Input placeholder={"Username"} />
-      <S.TextArea placeholder={"Please introduce yourself!"} />
-      <S.Input placeholder={"Email"} />
-      <S.Input placeholder={"Password"} />
+      <S.Input
+        name={"image"}
+        value={image}
+        onChange={handleChange}
+        placeholder={"URL of profile picture"}
+      />
+      <S.Input
+        name={"username"}
+        value={username}
+        onChange={handleChange}
+        placeholder={"Username"}
+      />
+      <S.TextArea
+        name={"bio"}
+        value={bio}
+        onChange={handleChange}
+        placeholder={"Please introduce yourself!"}
+      />
+      <S.Input
+        name={"email"}
+        value={email}
+        onChange={handleChange}
+        placeholder={"Email"}
+      />
+      <S.Input
+        name={"password"}
+        value={password}
+        onChange={handleChange}
+        placeholder={"Password"}
+      />
       <Button>Update Settings</Button>
     </S.Wrapper>
   );

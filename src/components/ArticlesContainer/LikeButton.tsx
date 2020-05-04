5252fdc9b8b314ff54b17598c29fe174250080e6
allenk <dev.allenk@gmail.com>
2020-05-04 19:36:47 +0900

    feat: like button hover시 스타일 추가

--- src/components/ArticlesContainer/LikeButton.tsx
+++ src/components/ArticlesContainer/LikeButton.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./likeButtonStyles";
 
 export default function LikeButton() {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <S.Button>
-      <S.Heart></S.Heart>
-      <S.Span>0</S.Span>
+    <S.Button
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      isHovered={isHovered}
+    >
+      <S.Heart isHovered={isHovered}></S.Heart>
+      <S.Span isHovered={isHovered}>0</S.Span>
     </S.Button>
   );
 }
--- src/components/ArticlesContainer/likeButtonStyles.ts
+++ src/components/ArticlesContainer/likeButtonStyles.ts
@@ -1,19 +1,24 @@
 import styled from "styled-components";
 import { MAIN_BLUE } from "@constants/colors";
 
-export const Button = styled.button`
+interface THovered {
+  isHovered?: boolean;
+}
+
+export const Button = styled.button<THovered>`
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 0 12px 0 16px;
-  border: 1px solid ${MAIN_BLUE};
+  border: 1px solid ${({ isHovered }) => (isHovered ? "none" : MAIN_BLUE)};
   border-radius: 5px;
+  background-color: ${({ isHovered }) => (isHovered ? MAIN_BLUE : "#fff")};
 `;
 
-export const Heart = styled.div`
+export const Heart = styled.div<THovered>`
   width: 8px;
   height: 8px;
-  background-color: ${MAIN_BLUE};
+  background-color: ${({ isHovered }) => (isHovered ? "#fff" : MAIN_BLUE)};
   transform: rotate(-45deg);
   &::before,
   &::after {
@@ -21,7 +26,7 @@ export const Heart = styled.div`
     position: absolute;
     width: 8px;
     height: 8px;
-    background-color: ${MAIN_BLUE};
+    background-color: ${({ isHovered }) => (isHovered ? "#fff" : MAIN_BLUE)};
     border-radius: 50%;
   }
   &::before {
@@ -30,7 +35,7 @@ export const Heart = styled.div`
   }
 `;
 
-export const Span = styled.span`
+export const Span = styled.span<THovered>`
   margin-left: 8px;
-  color: ${MAIN_BLUE};
+  color: ${({ isHovered }) => (isHovered ? "#fff" : MAIN_BLUE)};
 `;

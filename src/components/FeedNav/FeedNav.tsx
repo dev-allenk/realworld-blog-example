472e47dd088f7a0ce8856e8db31193ecffef3546
allenk <dev.allenk@gmail.com>
2020-05-05 13:07:40 +0900

    feat: 로그인 여부에 따라 적절한 FeedNav 그리도록 수정

--- src/components/FeedNav/FeedNav.tsx
+++ src/components/FeedNav/FeedNav.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import * as S from "./styles";
 import { useRouter } from "next/router";
 import { isEmpty } from "@utils";
@@ -14,24 +14,24 @@ export default function FeedNav() {
   const { isLoggedIn, username } = useSelector(
     (state: RootState) => state.auth
   );
-  useEffect(() => {
-    isEmpty(query) ? setActiveIndex(1) : setActiveIndex(0);
-  }, [query]);
 
-  const maps = [
-    {
-      path: `/?follow=${username}`,
-      tabName: "Your Feed",
-      queryKey: "follow",
-    },
-    { path: "/", tabName: "Global Feed", queryKey: "" },
-  ];
+  const maps = useMemo(() => {
+    const baseData = [
+      { path: `/?follow=${username}`, tabName: "Your Feed" },
+      { path: "/", tabName: "Global Feed" },
+    ];
+    return isLoggedIn ? baseData : [baseData[1]];
+  }, [isLoggedIn]);
+
+  useEffect(() => {
+    isLoggedIn && isEmpty(query) ? setActiveIndex(1) : setActiveIndex(0);
+  }, [query]);
 
   return (
     <S.FeedNav>
       {maps.map(({ path, tabName }, idx) => (
-        <Link href={path}>
-          <S.Tab key={tabName} isActive={activeIndex === idx} data-index={idx}>
+        <Link key={tabName} href={path}>
+          <S.Tab isActive={activeIndex === idx} data-index={idx}>
             {tabName}
           </S.Tab>
         </Link>

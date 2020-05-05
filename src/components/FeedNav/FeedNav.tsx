ccf644be99d4f91ff4456cc807fdc41c49404a65
allenk <dev.allenk@gmail.com>
2020-05-05 23:48:08 +0900

    feat: query에 맞는 active 탭 렌더링되는 FeedNav 기능 추가
    - 단일 컴포넌트로 재사용하고자 했으나 지나치게 복잡해지는 듯. 수정 필요.

--- src/components/FeedNav/FeedNav.tsx
+++ src/components/FeedNav/FeedNav.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useMemo } from "react";
 import * as S from "./styles";
 import { useRouter } from "next/router";
-import { isEmpty } from "@utils";
 import { useSelector } from "react-redux";
 import { RootState } from "@modules";
 import Link from "next/link";
@@ -16,21 +15,47 @@ export default function FeedNav() {
   );
 
   const maps = useMemo(() => {
-    const baseData = [
-      { path: `/?follow=${username}`, tabName: "Your Feed" },
-      { path: "/", tabName: "Global Feed" },
+    const mainPage = [
+      {
+        path: `/?follow=${username}`,
+        as: `/?follow=${username}`,
+        tabName: "Your Feed",
+        queryKey: "follow",
+      },
+      { path: "/", as: "", tabName: "Global Feed" },
     ];
-    return isLoggedIn ? baseData : [baseData[1]];
-  }, [isLoggedIn]);
+    const profilePage = [
+      {
+        path: `/profile/[username]?favorite=true`,
+        as: `/profile/${username}?favorite=true`,
+        tabName: "Favorited Articles",
+        queryKey: "favorite",
+      },
+      {
+        path: `/profile/[username]`,
+        as: `/profile/${username}`,
+        tabName: "My Articles",
+        queryKey: "username",
+      },
+    ];
+    const currentPage = pathname === "/" ? mainPage : profilePage;
+    return isLoggedIn ? currentPage : [currentPage[1]];
+  }, [isLoggedIn, query]);
 
   useEffect(() => {
-    isLoggedIn && isEmpty(query) ? setActiveIndex(1) : setActiveIndex(0);
+    //TODO: query 변화에 따라 active 탭을 렌더링하는 코드. 보다 단순한 코드로 수정 필요.
+    //firstTab이 고유하게 갖는 query값의 존재유무를 활용해서 현재 탭 파악.
+    const firstTabQuery = isLoggedIn ? maps[0].queryKey || "" : "";
+    const isCurrentQueryMatches = (t: string) => Object.keys(query).includes(t);
+    isLoggedIn && !isCurrentQueryMatches(firstTabQuery)
+      ? setActiveIndex(1)
+      : setActiveIndex(0);
   }, [isLoggedIn, query]);
 
   return (
     <S.FeedNav>
-      {maps.map(({ path, tabName }, idx) => (
-        <Link key={tabName} href={path}>
+      {maps.map(({ path, as, tabName }, idx) => (
+        <Link key={tabName} href={path} as={as}>
           <S.Tab isActive={activeIndex === idx} data-index={idx}>
             {tabName}
           </S.Tab>

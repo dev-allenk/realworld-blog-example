d952ee46b1f9ec3c259421af970e460c386f5a25
allenk <dev.allenk@gmail.com>
2020-05-05 14:49:17 +0900

    feat: Pagination active 상태의 스타일 추가

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "@modules";
 
 export default function Pagination() {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
   const articlesCount = useSelector(
     (state: RootState) => state.article.articlesCount
   );
@@ -13,7 +13,9 @@ export default function Pagination() {
     <S.Wrapper>
       <S.Button>{"<"}</S.Button>
       {pageCount.map((item, idx) => (
-        <S.Button>{idx + 1}</S.Button>
+        <S.Button key={idx + 1} isActive={idx === currentPage}>
+          {idx + 1}
+        </S.Button>
       ))}
       <S.Button>{">"}</S.Button>
     </S.Wrapper>
--- src/components/Pagination/styles.ts
+++ src/components/Pagination/styles.ts
@@ -1,17 +1,22 @@
 import styled from "styled-components";
 import { GREY_3, MAIN_BLUE } from "@constants/colors";
 
+interface TButton {
+  isActive?: boolean;
+}
+
 export const Wrapper = styled.nav`
   display: flex;
   justify-content: center;
   padding: 16px;
 `;
 
-export const Button = styled.button`
+export const Button = styled.button<TButton>`
   width: 32px;
   height: 32px;
-  border: 1px solid ${GREY_3};
-  background-color: #fff;
+  border: 1px solid ${({ isActive }) => (isActive ? MAIN_BLUE : GREY_3)};
+  background-color: ${({ isActive }) => (isActive ? MAIN_BLUE : "#fff")};
+  color: ${({ isActive }) => (isActive ? "#fff" : "#000")};
   &:hover {
     color: #fff;
     border: 1px solid ${MAIN_BLUE};

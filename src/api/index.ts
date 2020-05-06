7892cbb7fdac894ecef449eb8ef0397fcc0c7c96
allenk <dev.allenk@gmail.com>
2020-05-06 19:59:20 +0900

    chore: 클라이언트가 https 프로토콜 사용으로 api port number 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,7 +9,7 @@ import {
 const API_ENDPOINT =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000/api"
-    : "http://allenk-realworld-server.herokuapp.com/api";
+    : "http://allenk-realworld-server.herokuapp.com:80/api";
 
 const GET = "GET";
 const POST = "POST";

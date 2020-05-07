98313c3a7d927cc5fb369cf35df0ff0244ef6824
allenk <dev.allenk@gmail.com>
2020-05-07 17:55:41 +0900

    feat: user setting page 반응형 레이아웃 적용

--- src/components/UserSettings/styles.ts
+++ src/components/UserSettings/styles.ts
@@ -1,26 +1,20 @@
 import styled from "styled-components";
-import { GREY_4 } from "@constants/colors";
 import Flex from "../Layout/Flex";
+import { responsiveLayout } from "@components/Layout/Container";
+import { SInput, STextArea } from "@components/TextBox";
 
 const Wrapper = styled(Flex.VerticalCenter)`
   padding: 0 16px;
+  ${responsiveLayout}
 `;
 
-const Input = styled.input`
+const Input = styled(SInput)`
   margin: 8px 0;
-  padding: 0 16px;
-  width: 90%;
-  height: 42px;
-  border: 1px solid ${GREY_4};
-  border-radius: 5px;
 `;
-const TextArea = styled.textarea`
+
+const TextArea = styled(STextArea)`
   margin: 8px 0;
   padding: 16px;
-  width: 90%;
-  height: 200px;
-  border: 1px solid ${GREY_4};
-  border-radius: 5px;
 `;
 
 export default {

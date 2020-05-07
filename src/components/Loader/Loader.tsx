3a986abc77147358812b0b491f69e4b64456fafc
allenk <dev.allenk@gmail.com>
2020-05-07 20:09:02 +0900

    feat: Loader가 color값을 인자로 받을 수 있도록 수정

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,6 +4,7 @@ import * as S from "./styles";
 export interface TLoader {
   size?: string | number;
   unit?: string;
+  color?: string;
 }
 
 export default function Loader(props: TLoader) {
--- src/components/Loader/styles.ts
+++ src/components/Loader/styles.ts
@@ -7,13 +7,24 @@ const spin = keyframes`
     transform: rotate(360deg)
   }
 `;
+function hexToRgb(hex: string) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16),
+      ].join(",")
+    : null;
+}
 
 export const Loader = styled.div<TLoader>`
   margin: 16px auto;
   width: ${({ size = 40, unit = "px" }) => size + unit};
   height: ${({ size = 40, unit = "px" }) => size + unit};
-  border: 3px solid rgba(${MAIN_BLUE_RGB}, 0.3);
-  border-top-color: ${MAIN_BLUE};
+  border: 3px solid
+    rgba(${({ color }) => (color ? hexToRgb(color) : MAIN_BLUE_RGB)}, 0.3);
+  border-top-color: ${({ color }) => color || MAIN_BLUE};
   border-radius: 50%;
   animation: ${spin} 1s infinite;
 `;

854605dbf9d1968f7813b7505fcf3fca7ca6a89f
allenk <dev.allenk@gmail.com>
2020-05-07 17:36:13 +0900

    feat: signin/signup page 반응형 레이아웃 적용

--- src/components/LoginForm/styles.ts
+++ src/components/LoginForm/styles.ts
@@ -1,23 +1,28 @@
 import styled from "styled-components";
 import Flex from "../Layout/Flex";
 import { GREY_4 } from "@constants/colors";
+import { responsiveLayout } from "@components/Layout/Container";
 
 interface Validity {
   isValid: boolean;
   isEmpty: boolean;
 }
 
+const INPUT_MARGIN = "16px";
+const INPUT_BORDER = "1px";
+
 const Wrapper = styled(Flex.VerticalCenter)`
   padding: 16px;
+  ${responsiveLayout}
 `;
 
 const Input = styled.input<Validity>`
   position: relative;
-  margin: 8px 0;
+  margin: 8px ${INPUT_MARGIN};
   padding: 0 16px;
-  width: 90%;
+  width: calc(100% - (${INPUT_BORDER} + ${INPUT_MARGIN}) * 2);
   height: 42px;
-  border: 1px solid ${GREY_4};
+  border: ${INPUT_BORDER} solid ${GREY_4};
   border-radius: 5px;
   box-shadow: ${({ isEmpty, isValid }) =>
     isEmpty || isValid ? "none" : "0 0 0 4px red"};

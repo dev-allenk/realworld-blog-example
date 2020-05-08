85fff6741529bb259370611bb35a54087cc6d50e
allenk <dev.allenk@gmail.com>
2020-05-08 15:40:18 +0900

    refactor: 메시지를 변수로 분리

--- src/components/Article/Header.tsx
+++ src/components/Article/Header.tsx
@@ -13,6 +13,10 @@ import Modal from "@components/Modal";
 import { useRouter } from "next/router";
 import { TArticle } from "@types";
 
+const DELETE_COMFIRM_MESSAGE = "정말 삭제하시겠어요?";
+const DELETE_FAILURE_MESSAGE =
+  "삭제 과정에서 에러가 발생했습니다. 잠시 후에 다시 시도해주세요.";
+
 export default function ArticleHeader() {
   const { article, isLoading, isDeleting, error, username } = useSelector(
     ({ article, auth }: RootState) => ({
@@ -32,14 +36,12 @@ export default function ArticleHeader() {
   const isMyArticle = username === article.author?.username;
 
   const handleClick = () => {
-    if (confirm("정말 삭제하시겠어요?")) {
+    if (confirm(DELETE_COMFIRM_MESSAGE)) {
       dispatch(deleteArticle.request(article.slug));
 
       articleRef.current?.slug
         ? router.push("/")
-        : alert(
-            "삭제 과정에서 에러가 발생했습니다. 잠시 후에 다시 시도해주세요."
-          );
+        : alert(DELETE_FAILURE_MESSAGE);
     }
   };
   return (

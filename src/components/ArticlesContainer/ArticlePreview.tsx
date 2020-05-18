6243904235afc800b8d8f826ebaa5ebc5e66b55b
allenk <dev.allenk@gmail.com>
2020-05-18 20:17:56 +0900

    feat: like 버튼에 이벤트 추가

--- src/components/ArticlesContainer/ArticlePreview.tsx
+++ src/components/ArticlesContainer/ArticlePreview.tsx
@@ -5,6 +5,8 @@ import { TArticle } from "@types";
 import AuthorMeta from "@components/AuthorMeta";
 import path from "@constants/routingPaths";
 import Link from "next/link";
+import { useDispatch } from "react-redux";
+import { favoriteArticle } from "@modules/article";
 
 export default function ArticlePreview(props: TArticle) {
   const {
@@ -19,11 +21,22 @@ export default function ArticlePreview(props: TArticle) {
     favoritesCount,
     author: { username, bio, image, following },
   } = props;
+
+  const dispatch = useDispatch();
+
+  const updateFavorite = () => {
+    dispatch(favoriteArticle.request(slug));
+  };
+
   return (
     <S.PreviewItemWrapper>
       <S.Header>
         <AuthorMeta src={image} username={username} createdAt={createdAt} />
-        <LikeButton favorited={favorited} favoritesCount={favoritesCount} />
+        <LikeButton
+          favorited={favorited}
+          favoritesCount={favoritesCount}
+          onClick={updateFavorite}
+        />
       </S.Header>
       <Link href={path.article} as={path.articleAs(slug)}>
         <S.Link>
--- src/components/ArticlesContainer/LikeButton.tsx
+++ src/components/ArticlesContainer/LikeButton.tsx
@@ -4,15 +4,18 @@ import * as S from "./likeButtonStyles";
 interface Props {
   favorited: boolean;
   favoritesCount: number;
+  onClick(): void;
 }
 
 export default function LikeButton({
   favorited = false,
   favoritesCount = 0,
+  onClick,
 }: Props) {
   const [isHovered, setIsHovered] = useState(favorited);
   return (
     <S.Button
+      onClick={onClick}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       isHovered={isHovered}

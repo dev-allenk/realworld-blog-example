5d2989e61e76c785082dbb534d004e82a9522e76
allenk <dev.allenk@gmail.com>
2020-05-23 18:25:37 +0900

    feat: profileBanner에서 url query를 이용하여 프로필 얻는 기능 추가

--- src/components/ProfileBanner/ProfileBanner.tsx
+++ src/components/ProfileBanner/ProfileBanner.tsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as S from "./styles";
 import ProfileImage from "@components/ProfileImage";
 import Link from "next/link";
 import path from "@constants/routingPaths";
 import { useRouter } from "next/router";
 import { ButtonWithIcon } from "@components/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@modules";
+import { getProfile } from "@modules/profile";
 
 export default function ProfileBanner() {
   const {
     query: { author },
   } = useRouter();
+
+  const profile = useSelector((state: RootState) => state.profile);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProfile.request(author as string));
+  }, [author]);
+
   return (
     <S.Banner>
       <S.BannerInnerWrapper>
-        <ProfileImage large />
+        <ProfileImage large src={profile.image} />
         <S.Username>{author}</S.Username>
         <Link href={path.settings}>
           <S.ButtonWrapper>
